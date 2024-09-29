bf62d4c3166cbc8f7fdb58560c575e4f224d2452
minhasn <virtualleapltd@gmail.com>
2024-09-29 22:53:37 +0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,3 @@
-// userRoutes.js
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
@@ -8,6 +7,5 @@ router.get('/users/:id', userController.getUserById);
 router.post('/users', userController.createUser);
 router.put('/users/:id', userController.updateUser);
 router.delete('/users/:id', userController.deleteUser);
-router.post('/login', userController.loginUser);
 
 module.exports = router;
