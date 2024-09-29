1a9038158e2ea5e602b930857645f9a61d468695
minhasn <virtualleapltd@gmail.com>
2024-09-29 18:38:18 +0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,10 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
-  const { FullName, PhoneNo, City, Password, Email, Country } = req.body;
+  const { name, phone_number, city, password_hash, email, country, avatar_url } = req.body;
 
   try {
-    const user = await User.create({ FullName, PhoneNo, City, Password, Email, Country });
+    const user = await User.create({ name, phone_number, city, password_hash, email, country, avatar_url });
     res.status(201).json(user);
   } catch (error) {
     console.error('Database query error:', error.stack);
@@ -14,23 +14,23 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  const { PhoneNo, Password } = req.body;
+  const { phone_number, password_hash } = req.body;
 
   try {
-    const user = await User.findByPhoneNo(PhoneNo);
+    const user = await User.findByphone_number(phone_number);
 
     if (!user) {
       return res.status(400).json({ error: 'Invalid phone number or password' });
     }
 
-    const validPassword = await bcrypt.compare(Password, user.Password);
+    const validPassword = await bcrypt.compare(Password, user.password_hash);
 
     if (!validPassword) {
       return res.status(400).json({ error: 'Invalid phone number or password' });
     }
 
     // Return the UserId instead of User_Id
-    res.status(200).json({ UserId: user.UserId, FullName: user.FullName, City: user.City });
+    res.status(200).json({ UserId: user.UserId, name: user.name, city: user.city });
   } catch (error) {
     console.error('Database query error:', error.stack);
     res.status(500).json({ error: 'Database error' });
