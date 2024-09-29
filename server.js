965b0585f8fe85dcf6f378d093894cb90a56f1c4
Ahsan Javed <ahsanjaved1947@gmail.com>
2024-09-29 07:56:12 -0700

    modified:   server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require('./routes/userRoutes');
 const propertyRoutes = require('./routes/propertyRoutes');
 const propertyImageRoutes=require('./routes/propertyImageRoutes');
 const app = express();
