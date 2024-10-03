3ceaf71a13295b02b55a510675ef69ce5a46277b
minhasn <virtualleapltd@gmail.com>
2024-10-03 14:10:13 +0500

    Update property.js

--- models/property.js
+++ models/property.js
@@ -9,7 +9,7 @@ class Property {
           UserId, title, description, price, type, address, zipcode, city, geom, 
           bedrooms, washrooms, area, furnished, kitchen, water, electricity, status, category
         ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ST_SetSRID(ST_MakePoint(?, ?), 32643), 
-          ?, ?, ?, ?, ?, ?, ?, 'Unpaid', NULL)`,
+          ?, ?, ?, ?, ?, ?, ?, 'IsPaid', NULL)`,
         [userId, title, description, price, type, address, zipcode, city, geom.longitude, geom.latitude, bedrooms, washrooms, area, furnished, kitchen, water, electricity],
         (error, results) => {
           if (error) {
