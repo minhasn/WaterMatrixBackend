b5371462728ee410ae78ee417f65f580d8d26425
Ahsan Javed <ahsanjaved1947@gmail.com>
2024-10-04 09:02:59 -0700

    modified:   routes/propertyRoutes.js

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { addProperty, getProperties } = require('../controllers/propertyController');
+const { addProperty, getProperties, getPropertyById } = require('../controllers/propertyController');
 const multer = require('multer');
 const path = require('path');
 
