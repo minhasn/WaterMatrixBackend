d544ed1556f5a72341cf74a1e52fb9f20395b530
DurraizShahid <93601663+DurraizShahid@users.noreply.github.com>
2024-10-05 14:05:10 +0500

    Update database configuration to use Sequelize

--- config/db.js
+++ config/db.js
@@ -1,29 +1,27 @@
 // config/db.js
-const mysql = require('mysql2');
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-// Create MySQL connection pool
-const pool = mysql.createPool({
-  user: process.env.DATABASE_USER || 'root',
-  password: process.env.DATABASE_PASSWORD || 'RTYtGyNSeerXbGzCjgZKAwemeDsFAvao',
-  host: process.env.DATABASE_HOST || 'junction.proxy.rlwy.net',
-  port: process.env.DATABASE_PORT || 10063,
-  database: process.env.DATABASE_NAME || 'railway',
-  waitForConnections: true,
-  connectionLimit: 10, // You can adjust this based on your needs
-  queueLimit: 0, // 0 means no limit
-});
-
-// Check pool connection
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error('Database connection error:', err.stack);
-    return;
+// Create a Sequelize instance
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME || 'railway',
+  process.env.DATABASE_USER || 'root',
+  process.env.DATABASE_PASSWORD || 'RTYtGyNSeerXbGzCjgZKAwemeDsFAvao',
+  {
+    host: process.env.DATABASE_HOST || 'junction.proxy.rlwy.net',
+    port: process.env.DATABASE_PORT || 10063,
+    dialect: 'mysql',
+    logging: false, // Set to true for SQL query logging
   }
-  console.log('Connected to MySQL database successfully');
+);
 
-  // Release the connection back to the pool after the check
-  connection.release();
-});
+// Check the connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connected to MySQL database successfully');
+  })
+  .catch(err => {
+    console.error('Database connection error:', err);
+  });
 
-module.exports = pool;
+module.exports = sequelize;
