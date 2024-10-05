68bae00569e5ff6f9dc05e62af53c0c6112f83b8
minhasn <virtualleapltd@gmail.com>
2024-10-05 11:48:45 +0500

    Create I8_4326.js

---
+++ models/I8_4326.js
@@ -0,0 +1,52 @@
+// models/i8_4326.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const I8_4326 = sequelize.define('I8_4326', {
+  id_0: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  dimension: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  landuse_su: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  landuse__1: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  propertyno: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  sub_sector: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  sector: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  WKT: {
+    type: DataTypes.TEXT, // WKT (Well-Known Text) format for storing geometries
+    allowNull: true,
+  },
+  geometry: {
+    type: DataTypes.GEOMETRY('POLYGON'), // Assuming your geometry field stores polygons
+    allowNull: false,
+  },
+}, {
+  tableName: 'I8_4326',
+  timestamps: true, // Assuming you have created_at, updated_at
+});
+
+module.exports = I8_4326;
