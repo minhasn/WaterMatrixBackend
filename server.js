a6140f502046a3bf3aee83bdec351fd7cf2dd524
minhasn <virtualleapltd@gmail.com>
2024-10-06 12:50:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const propertyRoutes = require('./routes/propertyRoutes');
-const express = require('express');
 const app = express();
 const propertyRoutes = require('./routes/propertyRoutes');
 const propertyImageRoutes = require('./routes/propertyImageRoutes');
