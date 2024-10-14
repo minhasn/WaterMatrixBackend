e9b252173d71889337fb15109bdd708c7da9a9ef
DurraizShahid <93601663+DurraizShahid@users.noreply.github.com>
2024-10-14 16:20:56 +0500

    modified:   routes/propertyImageRoutes.js

--- routes/propertyImageRoutes.js
+++ routes/propertyImageRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const PropertyImageController = require('../controllers/imageController');
 const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 
 // Route to add an image
