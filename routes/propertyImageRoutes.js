0f9b693e795e9b368b8ca6603e1387e1e2ce83a3
DurraizShahid <93601663+DurraizShahid@users.noreply.github.com>
2024-10-14 16:12:53 +0500

    modified:   routes/propertyImageRoutes.js

--- routes/propertyImageRoutes.js
+++ routes/propertyImageRoutes.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const PropertyImageController = require('../controllers/imageController');
+app.use('/uploads', express.static('uploads'));
+const multer = require('multer');
+
 
 // Route to add an image
-router.post('/', PropertyImageController.addImage);
+router.post('/property-images', upload.single('image'), PropertyImageController.addImage);
 
 // Route to get images by property ID
 router.get('/property/:propertyId', PropertyImageController.getImages);
 
 // Route to delete an image
 router.delete('/:id', PropertyImageController.deleteImage);
+
 
 module.exports = router;
